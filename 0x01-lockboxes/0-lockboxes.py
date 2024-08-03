ca8501ea17cf837e32f42f62b711b749f0688e58
AIMAD KACEM <kacem.aimad@gmail.com>
2024-08-03 14:35:47 +0100

    0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""lock boxes Module"""
+
+
+def canUnlockAll(boxes):
+    """check if all boxes can be unlocked"""
+    n_boxes = len(boxes)
+    unlocked_boxes = dfs(boxes, 0)
+    if len(unlocked_boxes) == n_boxes:
+        return True
+    return False
+
+
+def dfs(boxes, start, visited=None):
+    """Depth-First Search Algorithm"""
+    if visited is None:
+        visited = set()
+    visited.add(start)
+    for key in boxes[start]:
+        if key not in visited:
+            dfs(boxes, key, visited)
+    return visited
