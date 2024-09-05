b8dd34efba8a543ae77047eda3da97626d762a99
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-05 23:46:52 +0100

    i got some errors from checker

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,12 +3,16 @@ const request = require('request');
 if (process.argv.length === 3) {
   const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
   request(url, function (err, response, body) {
-    if (!err) {
+    if (err) {
+      throw err;
+    } else {
       body = JSON.parse(body);
       const chars = body.characters;
       chars.forEach((char) => {
         request(char, function (err, response, body) {
-          if (!err) {
+          if (err) {
+            throw err;
+          } else {
             body = JSON.parse(body);
             console.log(body.name);
           }
