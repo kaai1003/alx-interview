bd5ff822f6ff1dc720b02a5aacd2d9f8c55d2951
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-05 23:57:18 +0100

    use async and promise

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,22 +2,25 @@
 const request = require('request');
 if (process.argv.length === 3) {
   const url = 'https://swapi-api.alx-tools.com/api/films/' + process.argv[2];
-  request(url, function (err, response, body) {
+  request(url, async (err, response, body) => {
     if (err) {
       throw err;
     } else {
       body = JSON.parse(body);
       const chars = body.characters;
-      chars.forEach((char) => {
-        request(char, function (err, response, body) {
-          if (err) {
-            throw err;
-          } else {
-            body = JSON.parse(body);
-            console.log(body.name);
-          }
+      for (const char of chars) {
+        await new Promise((resolve, reject) => {
+          request(char, function (err, response, body) {
+            if (err) {
+              reject(err);
+            } else {
+              body = JSON.parse(body);
+              console.log(body.name);
+              resolve();
+            }
+          });
         });
-      });
+      }
     }
   });
 }
