388e8cbabd1cd7ca0d8e4ea594d3845a6c01df2c
AIMAD KACEM <kacem.aimad@gmail.com>
2024-09-26 23:59:26 +0100

    0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -10,12 +10,12 @@ def island_perimeter(grid):
     for i in range(1, (h_len)):
         for j in range(1, (v_len)):
             if grid[i][j] == 1:
-                if j == 0 or grid[i][j - 1] == 0:
-                    perimeter += 1
-                if j == (v_len - 1) or grid[i][j + 1] == 0:
-                    perimeter += 1
                 if i == 0 or grid[i - 1][j] == 0:
                     perimeter += 1
                 if i == (h_len - 1) or grid[i + 1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j - 1] == 0:
+                    perimeter += 1
+                if j == (v_len - 1) or grid[i][j + 1] == 0:
                     perimeter += 1
     return perimeter
